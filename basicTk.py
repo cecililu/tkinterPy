67e3714679358492fe0b3231e63ac46a9af94fb7
cecil <cecilghimire@gmail.com>
2022-11-12 14:18:50 +0545

    basic tkinter window with button

--- basicTk.py
+++ basicTk.py
@@ -1,7 +1,12 @@
 from tkinter import *
+#creating root widget 
 root=Tk()
-
+# adding label to the root widget
 w=Label(root,text='hello')
+#pack method tells tk to fit size of the window to given text
 w.pack()
+#adding a button
+b=Button(text="Goodbye",command=root.destroy)
+b.pack()
 
 root.mainloop()
